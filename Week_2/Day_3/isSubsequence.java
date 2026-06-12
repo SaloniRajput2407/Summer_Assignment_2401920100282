9458d8a0940bdba89aa7a62539a81af07b07f84d
SaloniRajput2407 <salonirajput415@gmail.com>
2026-06-12 17:25:47 +0530

    Implement two-pointer approach for is subsequence

---
+++ Week_2/Day_3/isSubsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int i = 0, j = 0;
+        while(j<m && i<n){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else j++;
+
+        }
+        if(i==n) return true;
+        else return false;
+    }
+}
